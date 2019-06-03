d0d6e066f2022bd0a9249d2e63b0cfc8ef8c5b77
Derpy <jukodr@mail.ru>
2019-06-03 16:34:58 +0300

    In progress of making my own filter function.

--- Js/main.js
+++ Js/main.js
@@ -3,4 +3,21 @@ lightbox.option({
   'maxHeight': 600,
 })
 
+/*
 $('#search').hideseek();
+*/
+
+$('input').on('keyup', function(event) {
+  var $variable = $(this).val();
+  console.log($variable);
+
+  $('a').hide();
+
+  $('a').each(function (index, value) {
+    var $title = $(this).attr('data-title');
+    console.log($title);
+    if (($title).includes(($variable))) {
+      $(this).show();
+    }
+  })
+})
