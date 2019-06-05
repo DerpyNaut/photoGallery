613e9c1e7159071c770e42396bbd4a2d9ec7f85b
Derpy <jukodr@mail.ru>
2019-06-05 17:36:21 +0300

    Made filter, bit better, but requires tons of optimizing.

--- Js/main.js
+++ Js/main.js
@@ -8,16 +8,20 @@ $('#search').hideseek();
 */
 
 $('input').on('keyup', function(event) {
-  var $variable = $(this).val();
+  var $variable = $(this).val().toLowerCase();
   console.log($variable);
 
-  $('a').hide();
+  $('.galleryContainer a').css('display', 'none');
 
-  $('a').each(function (index, value) {
-    var $title = $(this).attr('data-title');
+  $('.galleryContainer a').each(function(index, value) {
+    var $title = $(this).attr('data-title').toLowerCase();
     console.log($title);
-    if (($title).includes($variable)) {
-      $(this).show();
-    }
-  });
+    if ($title.includes($variable)) {
+      $(this).css('display', 'inline');
+      $('.galleryContainer').prepend(this);
+    }});
+
+ // '''Lines of code to optimize.'''
+  $('.photo').remove();
+  $('.galleryContainer a').css('margin', '1rem');
 });
