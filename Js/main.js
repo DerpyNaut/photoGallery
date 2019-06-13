ef07f4cdec833fa87a99a4091e392e45c0117d7d
Derpy <jukodr@mail.ru>
2019-06-13 17:56:57 +0300

    Finished photo gallery

--- Js/main.js
+++ Js/main.js
@@ -1,7 +1,7 @@
 lightbox.option({
   'maxWidth': 1200,
   'maxHeight': 600,
-})
+});
 
 /*
 $('#search').hideseek();
@@ -11,17 +11,13 @@ $('input').on('keyup', function(event) {
   var $variable = $(this).val().toLowerCase();
   console.log($variable);
 
-  $('.galleryContainer a').css('display', 'none');
+  $('.galleryContainer .photo').hide();
 
-  $('.galleryContainer a').each(function(index, value) {
+  $('.galleryContainer .photo a').each(function(index, value) {
     var $title = $(this).attr('data-title').toLowerCase();
     console.log($title);
     if ($title.includes($variable)) {
-      $(this).css('display', 'inline');
-      $('.galleryContainer').prepend(this);
-    }});
-
- // '''Lines of code to optimize.'''
-  $('.photo').remove();
-  $('.galleryContainer a').css('margin', '1rem');
+      $(this).parent().show();
+    }}
+  );
 });
